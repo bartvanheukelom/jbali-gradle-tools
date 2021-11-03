ea5cffeebc3f1b1a3468b3a2af54bf3d0285ccbd
Bart van Heukelom <mail@bartvh.nl>
2021-11-03 18:06:14 +0100

    ProjectGroup childNames/childPaths

--- src/main/kotlin/ProjectGroup.kt
+++ src/main/kotlin/ProjectGroup.kt
@@ -1,12 +1,15 @@
 package org.jbali.gradle
 
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.provideDelegate
 import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
-open class ProjectGroup<T : ProjectWrapper>(
+abstract class ProjectGroup<T : ProjectWrapper>(
         private val groupProject: Project,
         private val type: KClass<out T>,
         private val wrapper: ProjectGroup<T>.(Project) -> T = { cp ->
@@ -17,11 +20,37 @@ open class ProjectGroup<T : ProjectWrapper>(
             }
         }
 ) {
-
+    protected annotation class Child
+    
+    private val logger get() = groupProject.logger
     val name: String get() = groupProject.name
-
+    
+    /**
+     * Get the names of children as declared with delegating properties in this group, e.g:
+     *
+     *     class Apps : ProjectGroup(...) {
+     *         @Child server by this
+     *         @Child worker by this
+     *     }
+     */
+    val childNames =
+        javaClass.kotlin.memberProperties
+            .filter { it.hasAnnotation<Child>() }
+            .map { it.name }
+            .also { logger.info("${javaClass.simpleName}.childNames = $it") }
+    
+    /**
+     * Paths of the children as determined in [childNames].
+     */
+    val childPaths by lazy {
+        childNames.map { n -> "${groupProject.path}:$n" }
+    }
+    
+    /**
+     * Wrapped children as determined by Gradle's [Project.getChildProjects]
+     */
     val children: Map<String, T> by lazy {
-        groupProject.logger.info("init ${this}.children")
+        logger.info("init ${this}.children")
         groupProject.childProjects.mapValues { (_, cp) ->
             try {
                 wrapper(cp)
@@ -29,11 +58,12 @@ open class ProjectGroup<T : ProjectWrapper>(
                 throw RuntimeException("Exception while wrapping $cp in ${type.simpleName}: $e", e)
             }
         }
+            .also { logger.info("${this}.children.size = ${it.size}") }
     }
 
     init {
         @Suppress("LeakingThis")
-        groupProject.logger.info("init $this: ${groupProject.childProjects.keys}")
+        logger.info("init $this: ${groupProject.childProjects.keys}")
     }
 
 //    fun configure(action: T.() -> Unit): Iterable<T> =
@@ -52,4 +82,6 @@ open class ProjectGroup<T : ProjectWrapper>(
             else -> throw NoSuchElementException("Cannot act as delegate for $property")
         }
 
+    abstract fun configure()
+    
 }
